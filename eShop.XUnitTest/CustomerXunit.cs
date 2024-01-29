85fe828f90d830b70f2d5d3ec3d2c4e72cf9b8dd
Cabuxito <briandigangi@hotmail.com>
2024-01-29 13:28:10 +0100

    +

--- eShop.XUnitTest/CustomerXunit.cs
+++ eShop.XUnitTest/CustomerXunit.cs
@@ -12,24 +12,24 @@ namespace eShop.XUnitTest
 {
     public class CustomerXunit
     {
-        //[Fact]
-        //public async Task TestCreateCustomerAsync()
-        //{
-        //    var _context = ContextCreater.CreateContext();
-        //    var _service = new CustomerServices(_context);
-        //    Arrange
+        [Fact]
+        public async Task TestCreateCustomerAsync()
+        {
+            var _context = ContextCreater.CreateContext();
+            var _service = new CustomerServices(_context);
+            //Arrange
 
-        //    CustomerDTO customerDTO = new CustomerDTO("thomas", "TheKrillz", "SkpMester@hotmail.com", "Alsgade");
+            CustomerDTO customerDTO = new CustomerDTO("thomas", "TheKrillz", "SkpMester@hotmail.com", "Alsgade");
 
-        //    Act
+            //Act
 
-        //    _service.CreateCustomerAsync(customerDTO);
+            _service.CreateCustomerAsync(customerDTO);
 
-        //    Assert
-        //    var actualCustomer = _context.Customers.ToList().Last();
+            //Assert
+            var actualCustomer = _context.Customers.ToList().Last();
 
-        //    Assert.Equal(customerDTO.Firstname, actualCustomer.Firstname);
-        //    Assert.Equal(customerDTO.Lastname, actualCustomer.Lastname);
-        //}
+            Assert.Equal(customerDTO.Firstname, actualCustomer.Firstname);
+            Assert.Equal(customerDTO.Lastname, actualCustomer.Lastname);
+        }
     }
 }
